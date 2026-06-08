32c037d9450d42e2ad4f4d9fe1d4740b3309fc0b
DistantCandle <loftuswp2@gmail.com>
2026-06-08 10:02:24 +0200

    Debug files automatic changes

--- intern-project/obj/Debug/net10.0/intern-project.AssemblyInfo.cs
+++ intern-project/obj/Debug/net10.0/intern-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("intern-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879ea4fbb0f400ad85cb263baf094bfc87dd7f8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f816ff5963e5ee72144883626922f9de4e4c7f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("intern-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("intern-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
